40d06557c1d01760cd6f718ea1fbce11deb78132
Jennifer Busca <jennifer.busca@gmail.com>
2019-04-01 12:05:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,3 @@
 def reverse_each_word(str)
-  words = str.split(" ")
-  new_arr = []
-  words.collect {|word| new_arr << word.reverse}
-return new_arr.join(" ")
+  words.split.collect {|word| word.reverse.join(" ")}
 end
